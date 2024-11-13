830cfa088ac79eae9d5c220f207a8ac2499a5625
Matheus Aurélio <matheusaurelio2004@gmail.com>
2024-11-12 23:01:58 -0300

    feat: add route register user

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import bcrypt from "bcryptjs"
 import { Request, Response, Router } from "express"
+import { prisma } from "./prisma/prismaClient"
 
 const router = Router()
 
 router.post("/user/register", async (request: Request, response: Response) => {
-  console.log("/user/register")
+  const { name, email, password } = request.body
+
+  if (!name || !email || !password) {
+    return response.status(400).send({ message: "Todos os campos são obrigatórios" })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 8)
+
+  await prisma.usuarios.create({
+    data: {
+      nome: name,
+      email,
+      senha: hashedPassword,
+    },
+  })
+
+  return response.status(201).send({ message: "Usuário cadastrado com sucesso" })
 })
 
 router.patch(
