4db31f7a7a7bd598fc7fe70441de159cdbbf6a43
Matheus Aurélio <matheusaurelio2004@gmail.com>
2025-02-15 03:33:04 -0300

    feat: :lock: add validators on signup and signin user

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import cors from "cors"
-import * as dotenv from "dotenv"
-import express from "express"
-import helmet from "helmet"
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
 
-import { router } from "./routes"
+import { router } from "./routes";
 
 dotenv.config()
 
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(helmet())
+app.use(express.json())
 app.use(
   cors({
     origin: "*",
@@ -19,8 +20,6 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 )
-app.use(express.json())
-
 app.use(router)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { isAdmin } from "./middlewares/isAdmin"
 import { isAuthenticated } from "./middlewares/isAuthenticated"
 import { isSupplier } from "./middlewares/isSupplier"
 import { limiter } from "./middlewares/rateLimit"
+import { userSchemaLogin, userSchemaSignup } from "./middlewares/validations/userSchemas"
+import { validateData } from "./middlewares/validations/validationMiddleware"
 import ProductController from "./product/product-controller"
 import SupplierController from "./supplier/supplier-controller"
 import UserController from "./user/user-controller"
@@ -11,8 +13,8 @@ import UserController from "./user/user-controller"
 const router = Router()
 
 router.get("/user/me", isAuthenticated, UserController.me)
-router.post("/user/register", limiter, UserController.create)
-router.post("/user/login", limiter, UserController.login)
+router.post("/user/register", [validateData(userSchemaSignup), limiter], UserController.create)
+router.post("/user/login", [validateData(userSchemaLogin), limiter], UserController.login)
 router.patch("/user/update/:userId", UserController.update)
 
 router.get("/products", ProductController.index)
